95e0c13f6ccb32dc3d4790e63ea511c880a20e33
Linerre <leon.errelin@outlook.com>
2021-02-16 00:48:02 +0800

    patch: font2; change liberation to firacode

--- config.def.h
+++ config.def.h
@@ -5,7 +5,14 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:style=Medium:pixelsize=18:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"Hack Nerd Font Mono:pixelsize=14:antialias=true:autohint=true",
+	"Inconsolata Nerd Font Mono:style=Medium:pixelsize=14:antilias=true;autohint=true",
+	"Liberation Mono:pixelsize=14:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
